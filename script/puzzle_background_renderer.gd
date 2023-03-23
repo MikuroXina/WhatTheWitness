4bc8d7cb2e5195ba122660abbc041705020262c5
MikuroXina <ryosukadnak@gmail.com>
2023-03-24 07:13:19 +0900

    Fix accessing field

--- script/puzzle_background_renderer.gd
+++ script/puzzle_background_renderer.gd
@@ -2,7 +2,7 @@ extends Control
 
 func _draw():
 	var canvas = Visualizer.PuzzleCanvas.new()
-	canvas.puzzle = Gameplay.puzzle
+	canvas.current_puzzle = Gameplay.puzzle
 	canvas.normalize_view(self.get_rect().size, 0.95, 0.8)
 	canvas.draw_puzzle(self)
 
