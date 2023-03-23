f94a9efb6838ca64d045f83a21fc3b7d1da05bb8
MikuroXina <ryosukadnak@gmail.com>
2023-03-23 18:26:16 +0900

    Refactor with scene var

--- script/warning_scene.gd
+++ script/warning_scene.gd
@@ -1,12 +1,13 @@
 extends Node2D
 
 @onready var checkbox = $MarginContainer/VBoxContainer/CheckBox
+@onready var mapScene = load("res://level_map.tscn")
 
 func _ready():
 	SaveData.load_all()
 	var setting = SaveData.get_setting()
 	if ('skip_spoiler' in setting):
-		get_tree().change_scene_to_packed(load("res://level_map.tscn"))
+		get_tree().change_scene_to_packed(mapScene)
 
 
 func _on_RichTextLabel2_meta_clicked(meta):
@@ -18,7 +19,7 @@ func _on_ContinueButton_pressed():
 		var setting = SaveData.get_setting()
 		setting['skip_spoiler'] = 1
 		SaveData.save_setting(setting)
-	get_tree().change_scene_to_packed(load("res://level_map.tscn"))
+	get_tree().change_scene_to_packed(mapScene)
 
 
 func _on_ExitButton_pressed():
